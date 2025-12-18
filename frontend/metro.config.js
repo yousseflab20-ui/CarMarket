4b12ae7b9f819a22a6ba7d425d3ab1c85c04343e
youssef lab20-u <yousseflab20@gmail.com>
2025-12-18 09:43:13 +0100

    setup for projets

---
+++ frontend/metro.config.js
@@ -0,0 +1,23 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
+    watchFolders: [
+        path.resolve(__dirname, '..', 'node_modules'),
+        path.resolve(__dirname, 'node_modules'),
+    ],
+    resolver: {
+        nodeModulesPaths: [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '..', 'node_modules'),
+        ],
+    },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
