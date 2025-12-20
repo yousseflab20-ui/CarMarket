56ca235e39b383ebdd90c0bcb5f52b369b876990
youssef lab20-u <yousseflab20@gmail.com>
2025-12-20 19:10:43 +0100

    push yourtable for User

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,12 +5,34 @@ import bcrypt from "bcrypt";
 const user = sequelize.define(
     "User",
     {
-        id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
-        name: { type: DataTypes.STRING, allowNull: false },
-        email: { type: DataTypes.STRING, allowNull: false, unique: true },
-        photo: { type: DataTypes.STRING, allowNull: false },
-        password: { type: DataTypes.STRING, allowNull: false },
-        role: { type: DataTypes.ENUM("USER", "ADMIN"), allowNull: false, defaultValue: "USER" },
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        photo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.ENUM("USER", "ADMIN"),
+            allowNull: false,
+            defaultValue: "USER"
+        },
     },
     {
         tableName: "User",
