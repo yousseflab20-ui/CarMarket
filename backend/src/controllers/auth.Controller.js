87554b74a5f318436a0ecc7e0401900f94853bef
youssef lab20-u <yousseflab20@gmail.com>
2025-12-20 19:23:06 +0100

    add your photo ind name for login and user

--- backend/src/controllers/auth.Controller.js
+++ backend/src/controllers/auth.Controller.js
@@ -1,11 +1,10 @@
 import user from "../models/User.js"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
-import { where } from "sequelize"
 export const login = async (req, res) => {
-    const { email, password } = req.body
+    const { email, password, photo, name } = req.body
 
-    if (!email || !password) {
+    if (!email || !password || !name || !photo) {
         return res.status(400).json({ message: "Email and password are required" });
     }
 
@@ -14,6 +13,8 @@ export const login = async (req, res) => {
         if (loginUser) {
             return res.status(400).json({ message: "User already exists" })
         }
+        await user.create({ email, password, photo, name });
+        return res.status(201).json({ message: "User registered successfully" })
     } catch (error) {
         return res.status(500).json({ message: "Server error", error: error.message })
     }
