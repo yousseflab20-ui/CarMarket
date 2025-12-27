7abf71435e518e3cffe2149ba5bd0d2cd5028321
yousseflab20-ui <yousseflab20@gmail.com>
2025-12-27 20:47:35 +0100

    add endpoint to fetch deletuser ind deletCar

--- backend/src/controllers/admin.Controller.js
+++ backend/src/controllers/admin.Controller.js
@@ -2,6 +2,7 @@ import user from "../models/User.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import car from "../models/Car.js";
+import { where } from "sequelize";
 dotenv.config();
 const JWT_TOKEN = process.env.JWT_TOKEN;
 export const createAdmin = async (req, res) => {
@@ -57,5 +58,39 @@ export const AllCar = async (req, res) => {
     }
   } catch (error) {
     return res.status(400).json({ message: "no valide allcar" });
+  }
+};
+export const deletCar = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const delet = await car.destroy({ where: { id } });
+    if (delet) {
+      return res.status(200).json({ message: "delet Car for Admin", delet });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "Car not found" });
+  }
+};
+
+export const allUser = async (req, res) => {
+  try {
+    const alluser = await user.findAll();
+    if (alluser) {
+      return res.status(200).json({ message: "User valide", alluser });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "no valide user" });
+  }
+};
+
+export const deletUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const alluser = await user.destroy({ where: { id } });
+    if (alluser) {
+      return res.status(200).json({ message: "delet User", alluser });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "add user" });
   }
 };
--- backend/src/router/adminRouter.js
+++ backend/src/router/adminRouter.js
@@ -2,11 +2,18 @@ import {
   createAdmin,
   loginAdmin,
   AllCar,
+  allUser,
+  deletCar,
+  deletUser,
 } from "../controllers/admin.Controller.js";
 import adminMiddleware from "../middlewares/adminMiddleware.js";
 import express from "express";
 const router = express.Router();
 router.post("/create-admin", createAdmin);
 router.post("/login-admin", loginAdmin);
-router.get("/All", adminMiddleware, AllCar);
+router.get("/AllCar", adminMiddleware, AllCar);
+router.delete("/all/:id", adminMiddleware, deletCar);
+router.get("/all/user", adminMiddleware, allUser);
+router.delete("/all/:id", adminMiddleware, deletCar);
+router.delete("/user/:id", adminMiddleware, deletUser);
 export default router;
