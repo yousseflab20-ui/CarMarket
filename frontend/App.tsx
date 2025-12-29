a24ff6620e2d70ef0caf91f357317f0fff9d74a3
yousseflab20-ui <yousseflab20@gmail.com>
2025-12-29 16:03:20 +0100

    frist react navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,45 +5,15 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, View, Text } from 'react-native';
-import {
-  SafeAreaProvider,
-  SafeAreaView,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
+import { NavigationContainer } from "@react-navigation/native"
 
 function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <StatusBar
-          backgroundColor="#ffffffff"
-          barStyle="dark-content"
-        />
-        <Text>Hello</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
