ab06000fe98027e47df035c04374a737377be879
youssef lab20-u <yousseflab20@gmail.com>
2025-12-30 01:36:00 +0100

    frist test your connection for database using unit test

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,5 +1,11 @@
-import { sequelize } from '../src/config/database.js';
+import sequelize from '../src/config/database.js';
 
+beforeAll(async () => {
+    await sequelize.authenticate();
+});
+afterAll(async () => {
+    await sequelize.close();
+});
 test('DB connection works', async () => {
     await expect(sequelize.authenticate()).resolves.not.toThrow();
 });
