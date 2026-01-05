28a2ee13add085185f6a2b9482d075ee929892d0
yousseflab20-ui <yousseflab20@gmail.com>
2026-01-05 02:13:37 +0100

    add your storage token for mmkv

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,13 @@
-import dotenv from "dotenv"
-dotenv.config()
-import { Sequelize } from "sequelize"
+import dotenv from "dotenv";
+dotenv.config();
+import { Sequelize } from "sequelize";
+// @ts-ignore
 const sequelize = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres"
-})
-export default sequelize
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+});
+export default sequelize;
