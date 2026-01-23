441f5acba128a7998e1be7ebfcd6482e9c5b2fc5
yousseflab20-ui <yousseflab20@gmail.com>
2026-01-23 01:18:29 +0100

    complet pull code

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: ['@react-native/babel-preset', 'nativewind/babel'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    'react-native-worklets/plugin',
-  ],
+  presets: ['module:metro-react-native-babel-preset', 'nativewind/babel'],
+  plugins: ['react-native-reanimated/plugin'],
 };
