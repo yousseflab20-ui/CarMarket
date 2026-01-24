2573582b29e86b29e5f8c112a931cf02e72407ed
yousseflab20-ui <yousseflab20@gmail.com>
2026-01-24 21:56:31 +0100

    add your edit

---
+++ frontend/src/service/car/api.ts
@@ -0,0 +1,37 @@
+
+import API from '../api';
+import { useAuthStore } from '../../store/authStore';
+import API_URL from '../../constant/URL';
+
+const baseURL = "http://10.0.2.2:5000/api";
+
+export const getAllCars = async () => {
+    const response = await API.get("/car/all");
+    return response.data;
+};
+
+export const addCar = async (carData: any) => {
+    try {
+        const token = useAuthStore.getState().token;
+        console.log("token", token);
+
+        const response = await fetch(`${API_URL}/car/add`, {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(carData),
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(errorText);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Add car error:", error);
+        throw error;
+    }
+};
