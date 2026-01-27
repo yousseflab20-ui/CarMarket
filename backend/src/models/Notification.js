11c807de658ca9a7e5b7777163c7f04cc1a86afc
youssef lab20-u <yousseflab20@gmail.com>
2026-01-27 11:42:47 +0100

    add your table notification

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+export const Notification = sequelize.define("Notification", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    messageId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    seen: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+});
