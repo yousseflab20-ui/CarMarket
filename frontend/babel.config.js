175cf1a6d9ef58bb32aa0ace8a2e828619da1c1a
yousseflab20-ui <yousseflab20@gmail.com>
2026-02-03 01:52:47 +0100

    fix error run android

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'nativewind/babel'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-  ],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: ['react-native-reanimated/plugin'],
 };
