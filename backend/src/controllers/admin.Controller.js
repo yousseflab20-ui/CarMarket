8acc06bb976f7de4e814cc9fc97c4f6557a7dd4e
Youssef Labnine <yousseflab20@gmail.com>
2026-03-05 20:16:24 +0000

    fixied crached backend

--- backend/src/controllers/admin.Controller.js
+++ backend/src/controllers/admin.Controller.js
@@ -124,17 +124,6 @@ export const getMessagesByConversation = async (req, res) => {
   }
 };
 
-export const getMessage = async (req, res) => {
-  try {
-    const getAll = await message.findAll();
-    if (getAll) {
-      return res.status(200).json({ message: "all Meesage", getAll });
-    }
-  } catch (error) {
-    return res.status(400).json({ message: "add your Meesage", error });
-  }
-};
-
 export const deletMessage = async (req, res) => {
   const { id } = req.params;
   try {
