7670276b7e7cd864e9216762644ec6d64e8b4b06
Youssef Labnine <yousseflab20@gmail.com>
2026-05-16 17:59:42 +0100

    feat:implementation table for save data report

---
+++ backend/src/models/Report.js
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Report = sequelize.define("Report", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+
+    // type of reported thing
+    targetType: {
+        type: DataTypes.ENUM("CAR", "USER", "POST"),
+        allowNull: false,
+    },
+
+    // id of the thing reported
+    targetId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+
+    reason: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+
+    status: {
+        type: DataTypes.ENUM("PENDING", "REVIEWED", "REJECTED"),
+        defaultValue: "PENDING",
+    },
+}, {
+    timestamps: true,
+    tableName: "Report",
+});
